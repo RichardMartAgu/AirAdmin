6be58580c5243e079db037184a99728b3b6f68fe
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2023-12-25 12:55:59 +0100

    Empezar activities index y listar aeropurerto. Añadir action bar.

---
+++ app/src/main/java/com/svalero/airadmin/view/AirportRegisterActivity.java
@@ -0,0 +1,7 @@
+package com.svalero.airadmin.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AirportRegisterActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/svalero/airadmin/view/IndexActivity.java
@@ -0,0 +1,29 @@
+package com.svalero.airadmin.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import com.svalero.airadmin.R;
+
+public class IndexActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+    public void goListAirports(View view){
+        Intent intent = new Intent(this, AirportListActivity.class);
+        startActivity(intent);
+    }
+}
